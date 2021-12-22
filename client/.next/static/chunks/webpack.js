188efdb7e46146bfa8fdc7e668249e7bec18e31d
Farouk <faroukkisuule55@gmail.com>
2021-12-21 20:11:17 -0500

    Added modal in state with antd that displays when the user has successfully registered

--- client/.next/static/chunks/webpack.js
+++ client/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "f9581211688bc8c8"; }
+/******/ 		__webpack_require__.h = function() { return "a10a33e61a3cbf49"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
